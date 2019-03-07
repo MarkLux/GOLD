f39a64e6504613d171ec02e3982af5a18716104f
MarkLux <marlx6590@163.com>
2019-03-07 13:09:13 +0800

    update: add hostname

--- serving/restful/main.go
+++ serving/restful/main.go
@@ -3,11 +3,16 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request) {
     fmt.Println("Hello World.")
     fmt.Fprintf(w, "Hello World.\n")
+    hostname, err := os.Hostname()
+    if err == nil {
+        fmt.Fprintf(w, "hostname: " + hostname + "\n")    
+    }
 }
 
 func main() {
