a10a279ab7a6f3a0e0e0078721ef759b1204b17d
hanxiao <1350690718@qq.com>
2019-05-05 16:16:01 +0800

    change gold_biz

--- api/tmp/gold_biz.go
+++ api/tmp/gold_biz.go
@@ -2,72 +2,26 @@ package gold
 
 import (
 	"github.com/MarkLux/GOLD/serving/rpc/goldrpc"
-	"github.com/MarkLux/GOLD/serving/wrapper/db"
 	"log"
 )
 
-/**
-  * function restful example
-  * show usage of rpc, db & cache
- */
-
-// the model of user
-// use annotation `bson` to control the key saved in db(for mongo).
-type UserModel struct {
-	Name string `bson:"name"`
-	Sex  string `bson:"sex"`
-	Mail string `bson:"mail"`
+func (s *GoldService) OnInit() {
+	log.Println("inited")
 }
 
 // the biz function
-func (s *GoldService) Handle(req *goldrpc.GoldRequest, rsp *goldrpc.GoldResponse) error {
+func (s *GoldService) OnHandle(req *goldrpc.GoldRequest, rsp *goldrpc.GoldResponse) error {
 	// get data from request
 	userName := req.Data["name"].(string)
-	hasCache := false
+	log.Println("userName: " + userName)
 
-	// cache example
-	cacheKey := "prefix_" + userName
-	u, err := s.CacheClient.Get(cacheKey)
-	if err != nil {
-		log.Println("fail to get info from cache, ", err)
-	}
-
-	// if got nothing from cache, then query the db.
-	if u == nil {
-		// db session example
-		dbSession, err := s.DbFactory.NewDataBaseSession("test", "user", "root", "pwd")
-		if err != nil {
-			log.Println("create db session failed, ", err)
-			return err
-		}
-		defer dbSession.Close()
-		// db query example
-		param := make(map[string]string)
-		param["data.name"] = userName
-		qUsers, err := dbSession.Query(db.GoldDBQuery{Param: param})
-		if err != nil {
-			log.Println("fail to query db, ", err)
-			return err
-		}
-		if len(qUsers) > 1 {
-			u = qUsers[0]
-			// reset the cache
-			err = s.CacheClient.Set(cacheKey, u, 300 * 1000)
-			if err != nil {
-				log.Println("fail to reset cache, ", err)
-			}
-		}
-		u = nil
-	}
-
-	hasCache = true
-
-	// build response
-	rsp.Data = make(map[string]interface{})
-	if u != nil {
-		rsp.Data["userModel"] = u
-		rsp.Data["hasCache"] = hasCache
-	}
+	greeting := "hello, " + userName
+	rsp.Data["greeting"] = greeting
 
 	return nil
+}
+
+func (s *GoldService) OnError(err error) bool {
+	log.Println("error!", err)
+	return false
 }
